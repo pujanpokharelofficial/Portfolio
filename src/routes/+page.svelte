eeb98f949812b8aa84147d0b661d2aee21aa9e1c
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-10 10:19:15 +0545

    Added a small hello world message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,2 @@
-<script>
-	import Background from "./background.svelte";
-
-</script>
-
-<Background />
+<h1 class="text-3xl font-bold font-mono">This site is under construction.</h1>
+<p>Hang in tight to see a beautifully crafted site.</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      colors:{
-        "circle-1": "#ACDD45",
-        "circle-2": "#359AF8",
-        "frost-blur": "#A0EDF880",
-      },
-    },
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
