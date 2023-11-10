5f5f847976f8e9758dba81a6ab2957c48bd8e328
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-10 22:13:43 +0545

    Completed the navigation styling

--- src/routes/global/nav.svelte
+++ src/routes/global/nav.svelte
@@ -9,16 +9,17 @@
 	};
 </script>
 
-<nav class="h-[100px] w-full px-standard flex justify-between items-center">
-	<div class="logo">Pujan Pokhrel</div>
-	<div class="navlinks uppercase text-[14px]">
-		{#each Object.entries(navlinks) as [displayName, href]}
-			<a class="gap-x-[20px]" {href}> {displayName} </a>
+<nav
+	class="h-[100px] w-full px-standard flex justify-between items-center border-b-2 border-[#E9E9E9] fixed top-0 left-0 bg-white"
+>
+	<div class="logo text-[24px] font-bold">Pujan Pokhrel</div>
+	<div class="navlinks uppercase text-[14px] text-[#595959]">
+		{#each Object.entries(navlinks) as [displayName, href], index (displayName)}
+			<a class={index != 0 ? 'ml-[20px]' : ''} {href}> {displayName} </a>
 		{/each}
 	</div>
-
 	<div class="downloadCV flex w-max justify-center items-center">
 		<img src="/svg/download.svg" alt="download icon" />
-		<span> download cv </span>
+		<span class="text-[#004994] uppercase text-[14px] ml-[10px]"> download cv </span>
 	</div>
 </nav>
