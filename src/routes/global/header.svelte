ccbbb6cb6b816df875c6a877a63d558fb669ca6a
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-12 01:22:18 +0545

    Header section complete

---
+++ src/routes/global/header.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let title: string;
+	export let viewAll: string | false = false;
+</script>
+
+<div class="heading uppercase flex px-block items-center space-x-standard">
+	<div class="title font-bold text-title text-black-medium">
+		{title}
+	</div>
+	<div class="line h-[2px] flex-1 bg-gray-dark" />
+	{#if viewAll}
+		<a href={viewAll} class="font-semibold text-sub-title text-black-light">view all</a>
+	{/if}
+</div>
---
+++ src/routes/home/about.svelte
@@ -0,0 +1,5 @@
+<script>
+	import Header from '../global/header.svelte';
+</script>
+
+<Header title="About me" viewAll="/contacts" />
