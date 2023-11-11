a58909c3dc16bdd0b061e32edc46bbba393b75fd
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-11 10:41:35 +0545

    Fixed the images

--- src/routes/global/footer.svelte
+++ src/routes/global/footer.svelte
@@ -1,4 +1,18 @@
 <script>
 </script>
 
-<p>Hello I am the footer</p>
+<footer
+	class="fixed bottom-0 left-0 min-h-[300px] justify-between items-center p-block bg-gray-extra-light w-full flex"
+>
+	<div class="logo text-[24px] font-bold">Pujan Pokharel</div>
+	<div class="socials">
+		<p class="uppercase">connect with me at :</p>
+		<div class="icons flex">
+			<img src="/images/twitter.png" alt="twitter-icon" />
+			<img src="/images/instagram.png" alt="instagram-icon" />
+			<img src="/images/facebook.png" alt="facebook-icon" />
+			<img src="/images/linkedin.png" alt="linkedin-icon" />
+			<img src="/images/youtube.png" alt="youtube-icon" />
+		</div>
+	</div>
+</footer>
