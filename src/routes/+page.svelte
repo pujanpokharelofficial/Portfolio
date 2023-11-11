3b2198bb0a4197b802a14f13873a8b96db545dd8
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-12 01:19:27 +0545

    fixed the right section height

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,23 @@
 <script>
-	import Hero from './hero.svelte';
+	import Hero from './home/hero.svelte';
+	import About from './home/about.svelte';
 </script>
 
 <svelte:head>
 	<title>Pujan Pokharel</title>
 	<meta
 		name="description"
-		content="Hello , I am Pujan Pokharel, A techie at heart, I possess comprehensive knowledge of graphics design, website and mobile application development, digital marketing, and content writing. Highly skilled and motivated youth professional with a strong background in the tech field with a keen eye for aesthetics and a passion for technology, I am committed to delivering innovative solutions and driving results for clients. Seeking opportunities to utilise my diverse skill set and contribute to the success of a forward-thinking organisation. Along with this, I am a young activist organising workshops, sessions, Bootcamps, Keynote sessions and conferences sharing my knowledge and inspiring others  to embrace their own potential. 
-I am willing to take risks, explore innovative ideas, and venture into uncharted territories. I have a passion for fusing technology and innovation to help solve problems in the real world.
-"
+		content="Hello , I am Pujan Pokharel, A techie at heart, I possess comprehensive knowledge of graphics design, website and mobile application development, digital marketing, and content writing.
+        Highly skilled and motivated youth professional with a strong background in the tech field with a keen eye for aesthetics and a passion for technology, I am committed to delivering innovative solutions and driving results for clients.
+        Seeking opportunities to utilise my diverse skill set and contribute to the success of a forward-thinking organisation.
+        Along with this, I am a young activist organising workshops, sessions, Bootcamps, Keynote sessions and conferences sharing my knowledge and inspiring others  to embrace their own potential.
+        I am willing to take risks, explore innovative ideas, and venture into uncharted territories.
+        I have a passion for fusing technology and innovation to help solve problems in the real world"
 	/>
 	<meta name="keyword" content="portfolio, pujan, pujan Pokharel" />
 </svelte:head>
 
-<div class="mt-section flex space-x-section">
+<div class="mt-section flex flex-col space-y-section">
 	<Hero />
+	<About />
 </div>
