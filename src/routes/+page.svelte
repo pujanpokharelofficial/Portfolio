beae5e6cba030a28c975e55bd02b01b91d96f606
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-12 00:04:42 +0545

    Update the title and page description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,17 @@
 	import Hero from './hero.svelte';
 </script>
 
+<svelte:head>
+	<title>Pujan Pokharel</title>
+	<meta
+		name="description"
+		content="Hello , I am Pujan Pokharel, A techie at heart, I possess comprehensive knowledge of graphics design, website and mobile application development, digital marketing, and content writing. Highly skilled and motivated youth professional with a strong background in the tech field with a keen eye for aesthetics and a passion for technology, I am committed to delivering innovative solutions and driving results for clients. Seeking opportunities to utilise my diverse skill set and contribute to the success of a forward-thinking organisation. Along with this, I am a young activist organising workshops, sessions, Bootcamps, Keynote sessions and conferences sharing my knowledge and inspiring others  to embrace their own potential. 
+I am willing to take risks, explore innovative ideas, and venture into uncharted territories. I have a passion for fusing technology and innovation to help solve problems in the real world.
+"
+	/>
+	<meta name="keyword" content="portfolio, pujan, pujan Pokharel" />
+</svelte:head>
+
 <div class="mt-section flex space-x-section">
 	<Hero />
 </div>
-
