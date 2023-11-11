02eee8001cf5c2ffdafb310841f3d0fb3827a0d4
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-11 11:32:12 +0545

    Complete the footer design

--- src/routes/global/footer.svelte
+++ src/routes/global/footer.svelte
@@ -1,18 +1,48 @@
 <script>
+	let firstColLinks = {
+		Home: '/',
+		'About Me': '/#about',
+		Honors: '/#honors'
+	};
+	let secondColLinks = {
+		Activities: '/#activities',
+		Projects: '/#projects',
+		'Contact Me': '/contacts'
+	};
 </script>
 
 <footer
-	class="fixed bottom-0 left-0 min-h-[300px] justify-between items-center p-block bg-gray-extra-light w-full flex"
+	class="fixed bottom-0 left-0 min-h-[300px] justify-between items-start p-block bg-gray-extra-light w-full flex"
 >
-	<div class="logo text-[24px] font-bold">Pujan Pokharel</div>
+	<div class="space-y-small">
+		<h1 class="logo text-title font-bold">Pujan Pokharel</h1>
+		<div class="contacts text-black-light text-para">
+			<p>contact@pujanpokharel.com.np</p>
+			<p>info.pujanpokharel@gmail.com</p>
+			<p>+977 9847090260</p>
+		</div>
+		<p class="text-black-medium font-bold text-para">ALL COPYRIGHTS ARE RESERVED © 2023</p>
+	</div>
 	<div class="socials">
-		<p class="uppercase">connect with me at :</p>
-		<div class="icons flex">
+		<p class="uppercase text-para text-black-medium">connect with me at :</p>
+		<div class="icons flex mt-small space-x-small">
 			<img src="/images/twitter.png" alt="twitter-icon" />
 			<img src="/images/instagram.png" alt="instagram-icon" />
 			<img src="/images/facebook.png" alt="facebook-icon" />
 			<img src="/images/linkedin.png" alt="linkedin-icon" />
 			<img src="/images/youtube.png" alt="youtube-icon" />
+		</div>
+	</div>
+	<div class="links_container flex space-x-standard text-para text-black-light uppercase">
+		<div class="links flex space-y-small flex-col">
+			{#each Object.entries(firstColLinks) as [displayName, href]}
+				<a {href}>{displayName}</a>
+			{/each}
+		</div>
+		<div class="links flex space-y-small flex-col">
+			{#each Object.entries(secondColLinks) as [displayName, href]}
+				<a {href}>{displayName}</a>
+			{/each}
 		</div>
 	</div>
 </footer>
