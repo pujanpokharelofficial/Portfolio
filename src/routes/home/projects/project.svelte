0fae5c0b059b985ca6821d83d472c9febd604ab6
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-14 15:31:34 +0545

    completed the design of the projects section.

--- src/routes/home/projects/project.svelte
+++ src/routes/home/projects/project.svelte
@@ -1,16 +1,19 @@
 <script>
 </script>
 
-<div class="project w-full h-full p-standard rounded-s border-[1px] border-gray-light group">
+<div
+	class="project w-full h-full p-standard rounded-md overflow-hidden border-[1px] border-gray-light group hover:bg-gray-extra-light hover:border-gray-dark"
+>
 	<div class="icon_link flex justify-between">
 		<img src="svg/blog-small.svg" alt="blog-Icon" class="h-[100px] w-auto" />
-		<div
-			class="viewall h-max w-max px-standard py-small rounded-s text-white font-semibold text-para bg-blue-light uppercase opacity-0 group-hover:opacity-100"
+		<a
+			href="somelink"
+			class="viewall h-max w-max px-standard py-small rounded-full text-white font-semibold text-para bg-blue-light uppercase opacity-0 group-hover:opacity-100"
 		>
 			view all
-		</div>
+		</a>
 	</div>
-	<div class="txt_content text-black-medium">
+	<div class="txt_content text-black-medium mt-standard">
 		<h1 class="title text-title font-bold uppercase">Highlighter</h1>
 		<p class="description text-para font-normal">
 			Highlighter is a very interesting project that, I created and I love it very much Highlighter
@@ -18,3 +21,9 @@
 		</p>
 	</div>
 </div>
+
+<style>
+	.project {
+		transition: 0.25s all;
+	}
+</style>
