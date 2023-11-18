ac55efc6e358c95cf888d311714741197fd442e8
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-18 20:46:27 +0545

    Changed the adapter to auto for prod testing.

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,12 @@
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		})
+		adapter: adapter()
 	}
 };
 
