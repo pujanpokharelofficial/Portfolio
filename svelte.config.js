6e20ad4d3a6202ef84e550bdff435508be234c4a
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-18 15:39:25 +0545

    using cloudflare as the deployment method.

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
 	}
 };
 
