a4706a01f94d10fb32cbc85975e839110a7f7c09
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-19 12:54:41 +0545

    Rendering honors from file in honors page.

--- src/routes/honors/+page.svelte
+++ src/routes/honors/+page.svelte
@@ -1,65 +1,10 @@
 <script lang="ts">
 	import Header from '../global/header.svelte';
 	import Honor from './honor.svelte';
-	import type { Thonor } from './type.ts';
-	let honors: Thonor[] = [
-		{
-			title: 'hero of the conference',
-			date: '27th december 2021',
-			location: 'devinagar chaupari, butwal',
-			description:
-				'i was awarded with this award for my extra ordinary works and errors in all the things that i have done for the one who game me those awards they are tryly appriciative of the work',
-			image: '/images/honors.jpg'
-		},
-		{
-			title: 'hero of the conference',
-			date: '27th december 2021',
-			location: 'devinagar chaupari, butwal',
-			description:
-				'i was awarded with this award for my extra ordinary works and errors in all the things that i have done for the one who game me those awards they are tryly appriciative of the work',
-			image: '/images/honors.jpg'
-		},
-		{
-			title: 'hero of the conference',
-			date: '27th december 2021',
-			location: 'devinagar chaupari, butwal',
-			description:
-				'i was awarded with this award for my extra ordinary works and errors in all the things that i have done for the one who game me those awards they are tryly appriciative of the work',
-			image: '/images/honors.jpg'
-		},
-		{
-			title: 'hero of the conference',
-			date: '27th december 2021',
-			location: 'devinagar chaupari, butwal',
-			description:
-				'i was awarded with this award for my extra ordinary works and errors in all the things that i have done for the one who game me those awards they are tryly appriciative of the work',
-			image: '/images/honors.jpg'
-		},
-		{
-			title: 'hero of the conference',
-			date: '27th december 2021',
-			location: 'devinagar chaupari, butwal',
-			description:
-				'i was awarded with this award for my extra ordinary works and errors in all the things that i have done for the one who game me those awards they are tryly appriciative of the work',
-			image: '/images/honors.jpg'
-		},
-		{
-			title: 'hero of the conference',
-			date: '27th december 2021',
-			location: 'devinagar chaupari, butwal',
-			description:
-				'i was awarded with this award for my extra ordinary works and errors in all the things that i have done for the one who game me those awards they are tryly appriciative of the work',
-			image: '/images/honors.jpg'
-		},
-		{
-			title: 'hero of the conference',
-			date: '27th december 2021',
-			location: 'devinagar chaupari, butwal',
-			description:
-				'i was awarded with this award for my extra ordinary works and errors in all the things that i have done for the one who game me those awards they are tryly appriciative of the work',
-			image: '/images/honors.jpg'
-		}
-	];
+
+	import data from '$lib/data/honors.json';
+
+	$: honors = data;
 </script>
 
 <div class="main_container min-h-screen pb-block mt-[200px]">
