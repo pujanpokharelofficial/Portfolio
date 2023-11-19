4df2370cac9f3bb2c2be279f27a8a97ec9d06aab
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-19 13:01:05 +0545

    Added active nav, file based content and auto scroll top in activities section.

--- src/routes/activities/+page.svelte
+++ src/routes/activities/+page.svelte
@@ -3,35 +3,20 @@
 	import Activity from './activity.svelte';
 	import type { Tactivities } from './type.ts';
 
-	let activities: Tactivities[] = [
-		{
-			title: 'Some test activity',
-			description:
-				'Here is something that you need to know, How can I work. Here is something that you need to know, How can I work. Here is something that you need to know, How can I work. Here is something that you need to know, How can I work.',
-			date: '13th jan 2021',
-			image: '/images/honors.jpg',
-			location: 'Kapilbastu, aashish ko ghar'
-		},
-		{
-			title: 'Some test activity',
-			description:
-				'Here is something that you need to know, How can I work. Here is something that you need to know, How can I work. Here is something that you need to know, How can I work. Here is something that you need to know, How can I work.',
-			date: '13th jan 2021',
-			image: '/images/honors.jpg',
-			location: 'Kapilbastu, aashish ko ghar'
-		},
-		{
-			title: 'Some test activity',
-			description:
-				'Here is something that you need to know, How can I work. Here is something that you need to know, How can I work. Here is something that you need to know, How can I work. Here is something that you need to know, How can I work.',
-			date: '13th jan 2021',
-			image: '/images/honors.jpg',
-			location: 'Kapilbastu, aashish ko ghar'
-		}
-	];
+	import data from '$lib/data/activities.json';
+	import { onMount } from 'svelte';
+	import { activeNav } from '../../store';
+
+	$: activities = data;
+
+	let main_container: HTMLElement;
+	onMount(() => {
+		activeNav.update(() => 'activities');
+		main_container.scrollIntoView();
+	});
 </script>
 
-<div class="main_container mt-[200px]">
+<div bind:this={main_container} class="main_container mt-[200px]">
 	<Header title="activities" />
 	<div class="activities w-full h-max p-block flex flex-col space-y-block items-center">
 		{#each activities as activity, index (activity)}
