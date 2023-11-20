1aaa4b1e5c256f8e86422ecd19f09a5f04c2f2a6
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-20 16:22:55 +0545

    fixed the x overflow.

--- src/routes/global/nav.svelte
+++ src/routes/global/nav.svelte
@@ -20,7 +20,9 @@
 	class="nav h-[100px] w-full px-block flex justify-between items-center border-b-2 border-gray-light fixed top-0 left-0 z-50"
 >
 	<a href="/#home" class="logo text-title font-bold">Pujan Pokharel</a>
-	<div class="navlinks uppercase text-info text-black-light flex space-x-small PD:space-x-standard">
+	<div
+		class="navlinks uppercase text-info text-black-light SD:flex space-x-small PD:space-x-standard hidden"
+	>
 		{#each Object.entries(navlinks) as [displayName, href]}
 			<a
 				class={(displayName == $activeNav && 'text-blue-dark font-medium') ||
@@ -35,11 +37,38 @@
 	<a
 		href="/pdf/resume.pdf"
 		download="Pujan Pokharel CV.pdf"
-		class="downloadCV flex w-max justify-center items-center cursor-pointer"
+		class="downloadCV w-max justify-center items-center SD:flex hidden cursor-pointer"
 	>
 		<img src="/svg/download.svg" alt="download icon" />
 		<span class="text-blue-dark uppercase text-[14px] ml-[10px]"> download cv </span>
 	</a>
+
+	<div
+		class="side_nav absolute w-full bg-red-300 left-0 top-0 h-screen p-block hidden flex-col justify-between"
+	>
+		<div
+			class="navlinks uppercase text-info text-black-light flex space-x-small PD:space-x-standard"
+		>
+			{#each Object.entries(navlinks) as [displayName, href]}
+				<a
+					class={(displayName == $activeNav && 'text-blue-dark font-medium') ||
+						'text-black-light font-medium'}
+					{href}
+					on:click={() => updateNavActive(displayName)}
+				>
+					{displayName}
+				</a>
+			{/each}
+		</div>
+		<a
+			href="/pdf/resume.pdf"
+			download="Pujan Pokharel CV.pdf"
+			class="downloadCV flex w-max justify-center items-center cursor-pointer"
+		>
+			<img src="/svg/download.svg" alt="download icon" />
+			<span class="text-blue-dark uppercase text-[14px] ml-[10px]"> download cv </span>
+		</a>
+	</div>
 </nav>
 
 <style>
